55aeaea4bf25d9e96d2d0bbbc7b254c8f87de8b4
okumu87 <okumu793@gmail.com>
2026-09-11 22:56:34 +0200

    added habits dynamically to the habit list container. Added a button to add new habits.

--- pulseboard.js
+++ pulseboard.js
@@ -42,3 +42,34 @@ const healthHabits = habits.filter(habit => habit.category === "Health");
 const averageTargetPerDay = healthHabits.reduce((total, habit) => total + habit.targetPerDay, 0) / healthHabits.length;
 
 console.log(averageTargetPerDay)
+
+// functions 
+
+function renderHabits (){
+    const habitContainer = document.getElementById("habit-list");
+    habitContainer.innerHTML = habits.map(habit => 
+        `<div>${habit.name} (${habit.category}) — Target: ${habit.targetPerDay} ${habit.unit} ${habit.loggedToday >= habit.targetPerDay ? '✅' : ''}<button class="log-btn" data-id="${habit.id}">+1</button></div>`
+    ).join('');
+    
+}
+
+renderHabits();
+
+
+document.getElementById("test-btn").addEventListener("click", () => {
+  console.log("Button clicked!");
+});
+
+const habitContainer = document.getElementById("habit-list");
+
+habitContainer.addEventListener("click", (event) => {
+        if(event.target.classList.contains("log-btn")){
+           const habitId = Number(event.target.dataset.id);
+
+           const habit = habits.find(habit => habit.id === habitId);
+              if(habit){
+                  habit.loggedToday = (habit.loggedToday || 0) + 1;
+                  renderHabits();
+              }
+        }
+    });
