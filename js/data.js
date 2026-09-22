c82d43600bba171b9a7d5d94bf84f7c9b1d3e2fb
okumu87 <okumu793@gmail.com>
2026-09-22 13:07:07 +0200

    working on export import of files

---
+++ js/data.js
@@ -0,0 +1,44 @@
+// the array of data
+
+const defaultHabits = [
+    {id: 0, name: "Drink water", 
+        category: "Health", 
+        targetPerDay: 5, 
+        loggedToday: 0,
+        unit: "glasses"},
+    {id: 1, 
+        name: "Wash hands", 
+        category: "Productivity", 
+        targetPerDay: 8, 
+        loggedToday: 0,
+        unit: "times"},
+    {id: 2, 
+        name: "Pray", 
+        category: "Mindfulness", 
+        targetPerDay: 6, 
+        loggedToday: 0,
+        unit: "times"},
+    {id: 3, 
+        name: "Read books", 
+        category: "Others", 
+        targetPerDay: 4, 
+        loggedToday: 0,
+        unit: "pages"}
+]
+
+// local storage
+
+    export function saveHabits(){
+        localStorage.setItem('habits', JSON.stringify(habits));
+    }
+
+    function loadHabits (){
+        const saved = localStorage.getItem('habits');
+        if(saved){
+            return JSON.parse(saved);
+        }else{
+            return defaultHabits;
+        }
+    }
+
+    export let habits = loadHabits();
--- js/pulseboard.js
+++ js/pulseboard.js
@@ -1,71 +1,11 @@
-const defaultHabits = [
-    {id: 0, name: "Drink water", 
-        category: "Health", 
-        targetPerDay: 5, 
-        loggedToday: 0,
-        unit: "glasses"},
-    {id: 1, 
-        name: "Wash hands", 
-        category: "Productivity", 
-        targetPerDay: 8, 
-        loggedToday: 0,
-        unit: "times"},
-    {id: 2, 
-        name: "Pray", 
-        category: "Mindfulness", 
-        targetPerDay: 6, 
-        loggedToday: 0,
-        unit: "times"},
-    {id: 3, 
-        name: "Read books", 
-        category: "Others", 
-        targetPerDay: 4, 
-        loggedToday: 0,
-        unit: "pages"}
-]
+import { habits, saveHabits } from "./data";
+
 
 let currentWeather = null;
 
 
-// const habitNames = habits.map(habit => habit.name);
 
-// console.log(habitNames)
-
-// const mindfullnessHabits = habits.filter(habit=>habit.category === "Mindfulness");
-
-// console.log(mindfullnessHabits)
-
-// const habitById = habits.find(habit => habit.id === 3);
-
-// console.log(habitById) 
-
-// const totalDailyTarget = habits.reduce((total, habit) => total + habit.targetPerDay, 0);
-
-// console.log(totalDailyTarget)
-
-
-// const healthHabits = habits.filter(habit => habit.category === "Health");
-// const averageTargetPerDay = healthHabits.reduce((total, habit) => total + habit.targetPerDay, 0) / healthHabits.length;
-
-// console.log(averageTargetPerDay)
-
-
-  // local storage
-
-    function saveHabits(){
-        localStorage.setItem('habits', JSON.stringify(habits));
-    }
-
-    function loadHabits (){
-        const saved = localStorage.getItem('habits');
-        if(saved){
-            return JSON.parse(saved);
-        }else{
-            return defaultHabits;
-        }
-    }
-
-    let habits = loadHabits();
+  
 
 // functions 
 
@@ -78,7 +18,7 @@ function renderHabits (){
         : `<p>Loading weather...</p>`;
  
 
-    habitHtml = habits.map(habit => 
+    const habitHtml = habits.map(habit => 
         `<div>${habit.name} (${habit.category}) — Target: ${habit.targetPerDay} ${habit.unit} ${habit.loggedToday >= habit.targetPerDay ? '✅' : ''} ${habit.loggedToday >= habit.targetPerDay ? 0 : habit.targetPerDay - habit.loggedToday}  <button class="log-btn" data-id="${habit.id}">+1</button> <button class="delete-btn" data-id="${habit.id}">🗑️</button></div>`
     ).join('');
 
